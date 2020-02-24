a649f1a30953327737703c906158da9b6d10377e
Vitrant <l.vitrant@rubika-edu.com>
2020-02-24 16:57:03 +0100

    Comportement Ennemi 01
    L'Expérience ratée, détecte, suit et dash vers le joueur.

---
+++ ZeldaLike/Assets/Scripts/En develloppement/Louis/AnimScr_Ennemis_Following.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ennemis;
+
+public class AnimScr_Ennemis_Following : StateMachineBehaviour
+{
+    public Ennemis_Fondation mySelf = null;
+    public Transform player;
+    public float speed;
+    private Animator Animator;
+
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        mySelf = GameObject.Find("Bot").GetComponent<Ennemis_Fondation>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        speed = mySelf._movementSpeed;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if ((player.transform.position - mySelf.transform.position).magnitude < mySelf._detectionRange)
+        {
+            mySelf.transform.position = Vector2.MoveTowards(mySelf.transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
---
+++ ZeldaLike/Assets/Scripts/En develloppement/Louis/Ennemis_Fondation.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Ennemis
+{
+    public class Ennemis_Fondation : MonoBehaviour
+    {
+        private Animator animator;
+        [Header("HP")]
+        public int life = 3;
+        [Space(10)]
+        [Header("Target")]
+        public Transform target = null;
+        public float _detectionRange = 10f;
+        public float _attackRange = 3f;
+        public float _movementSpeed = 5f;
+        public float _dashSpeed = 10f;
+
+        private void Start()
+        {
+            animator = this.gameObject.GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            Living(life);
+        }
+
+        void Living(int life)
+        {
+            if(life <= 0)
+            {
+                StartCoroutine(Dying());
+            }
+        }
+
+
+        IEnumerator Dying()
+        {
+            //Death anim
+
+            yield return new WaitForSeconds(1.5f);
+
+            Destroy(this.gameObject);
+
+            yield return 0;
+        }
+    }
+
+}
