7816fe0c2cdf047126bbd6b63013f7a9cad41d94
LucV <l.vidal@rubika-edu.com>
2020-03-24 16:34:07 +0100

    Flaque et Dummy presque finis

---
+++ ZeldaLike/Assets/Scripts/En develloppement/Luc/Scr_LD_FlaqueRemoval.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Scr_LD_FlaqueRemoval : MonoBehaviour
+{
+    public GameObject _dangerousBloc;
+    public Tilemap _tilemap;
+    public Vector3Int _blocPos;
+    private bool _flaqueGettingDestroyed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _tilemap = GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_flaqueGettingDestroyed == true)
+        {
+            _blocPos = _tilemap.WorldToCell(_dangerousBloc.transform.position);
+            _tilemap.SetTile(_blocPos, null);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Environment"))
+        {
+            _flaqueGettingDestroyed = true;
+            _dangerousBloc = collision.gameObject;
+        }
+    } 
+}
