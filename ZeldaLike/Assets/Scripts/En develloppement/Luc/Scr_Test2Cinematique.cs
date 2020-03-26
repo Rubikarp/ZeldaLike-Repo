8c2f5b25eb96f11cfccdc7c7298f61d90af50732
LucV <l.vidal@rubika-edu.com>
2020-03-26 14:23:50 +0100

    Premier Test cinematique + dialogues

---
+++ ZeldaLike/Assets/Scripts/En develloppement/Luc/Scr_Test2Cinematique.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Scr_Test2Cinematique : MonoBehaviour
+{
+    public PlayableDirector _playableDirector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            _playableDirector.Play();
+        }
+    }
+}
---
+++ ZeldaLike/Assets/Scripts/En develloppement/Luc/Scr_Test_Cinematique.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Management;
+
+public class Scr_Test_Cinematique : MonoBehaviour
+{
+
+    public Dialog _dialogue;
+    private int _talkCount;
+    private float _talkDelay;
+    private bool _isTalking;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<Scr_DialogManager>().StartDialogue(_dialogue);
+        _isTalking = true;
+        _talkDelay = 5;
+        _talkCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_isTalking == true && _talkDelay > 0)
+        {
+            _talkDelay -= Time.deltaTime;
+        }
+        else if (_isTalking == true && _talkDelay <= 0)
+        {
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            _talkCount += 1;
+            _talkDelay = 5;
+        }
+
+        if (_isTalking == true && _talkCount > _dialogue.sentences.Length)
+        {
+            _isTalking = false;
+            _talkCount = 0;
+        }
+    }
+}
