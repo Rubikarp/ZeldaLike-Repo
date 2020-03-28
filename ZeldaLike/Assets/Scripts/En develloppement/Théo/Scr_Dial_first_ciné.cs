82dc97a37996f233da6cec191605d7dbc8c3c27c
Théo <t.valet@rubika-edu.com>
2020-03-28 10:42:57 +0100

    Première cinématique du jeu

---
+++ ZeldaLike/Assets/Scripts/En develloppement/Théo/Scr_Dial_first_ciné.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Management;
+
+public class Scr_Dial_first_ciné : MonoBehaviour
+{
+    public Dialog _dialog;
+    public List<float> _delai;
+    public List<bool> _actifDialog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<Scr_DialogManager>().StartDialogue(_dialog);
+        _actifDialog[0] = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Soldat
+        if (_delai[0] > 0 && _actifDialog[0] == true)
+        {
+            _delai[0] -= Time.deltaTime;
+        }
+        else if (_delai[0] <= 0 && _actifDialog[0] == true)
+        {
+            Debug.Log("Merde");
+            _dialog.name = "Soldat";
+            FindObjectOfType<Scr_DialogManager>().StartDialogue(_dialog);
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            _actifDialog[0] = false;
+            _actifDialog[1] = true;
+        }
+
+        // Nahua
+        if (_delai[1] > 0 && _actifDialog[1] == true)
+        {
+            _delai[1] -= Time.deltaTime;
+        }
+        else if (_delai[1] <= 0 && _actifDialog[1] == true)
+        {
+            _dialog.name = "Nahua";
+            FindObjectOfType<Scr_DialogManager>().StartDialogue(_dialog);
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            _actifDialog[1] = false;
+            _actifDialog[2] = true;
+
+        }
+
+        // Soldat
+        if (_delai[2] > 0 && _actifDialog[2] == true)
+        {
+            _delai[2] -= Time.deltaTime;
+        }
+        else if (_delai[2] <= 0 && _actifDialog[2] == true)
+        {
+            _dialog.name = "Soldat";
+            FindObjectOfType<Scr_DialogManager>().StartDialogue(_dialog);
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            _actifDialog[2] = false;
+            _actifDialog[3] = true;
+        }
+
+        // Nahua
+        if (_delai[3] > 0 && _actifDialog[3] == true)
+        {
+            _delai[3] -= Time.deltaTime;
+        }
+        else if (_delai[3] <= 0 && _actifDialog[3] == true)
+        {
+            _dialog.name = "Nahua";
+            FindObjectOfType<Scr_DialogManager>().StartDialogue(_dialog);
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            _actifDialog[3] = false;
+            _actifDialog[4] = true;
+        }
+
+        // Dernier Délai
+        if (_delai[4] > 0 && _actifDialog[4] == true)
+        {
+            _delai[4] -= Time.deltaTime;
+        }
+        else if (_delai[4] <= 0 && _actifDialog[4] == true)
+        {
+            FindObjectOfType<Scr_DialogManager>().DisplayNextSentence();
+            _actifDialog[4] = false;
+        }
+    }
+}
