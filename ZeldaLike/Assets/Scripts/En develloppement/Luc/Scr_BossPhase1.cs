58db5bc0d7406ca44dd5105107e21af4bb627c12
LucV <l.vidal@rubika-edu.com>
2020-04-03 12:20:26 +0200

    Boss Phase 1 un peu plus avancé

--- ZeldaLike/Assets/Scripts/En develloppement/Luc/Scr_BossPhase1.cs
+++ ZeldaLike/Assets/Scripts/En develloppement/Luc/Scr_BossPhase1.cs
@@ -9,15 +9,36 @@ public class Scr_BossPhase1 : MonoBehaviour
     private float _delay;
     private bool _canGoDelay;
     private int _randomAction;
+    public Transform _mySelf;
+    private GameObject _player;
 
+    [Header("Renforts")]
     public List<GameObject> _renforts;
     public List<Transform> _renfortsSpawns;
+
+    [Header("Tirs")]
+    public GameObject _bullet;
+    public float _couvertureSpeed;
+    private float _couvertureDuration;
+    public float _couvertureDurationOrigin;
+    public Transform _bulletContainer;
+    public float _shootingAllonge;
+
+    [Header("Attaque au CaC")]
+    public float _attackRange;
+
+    [Header("Grenade")]
+    public GameObject _grenade;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        _player = GameObject.FindGameObjectWithTag("Player"); 
         _actionActive = false;
         _delay = _delayBetweenActions;
         _canGoDelay = false;
+        _couvertureDuration = _couvertureDurationOrigin;
     }
 
     // Update is called once per frame
@@ -47,6 +68,26 @@ public class Scr_BossPhase1 : MonoBehaviour
                     Renforts();
                     _actionActive = true;
                     break;
+
+                case 1:
+                    TirDeCouverture();
+                    _actionActive = true;
+                    break;
+
+                case 2:
+                    Grenade();
+                    _actionActive = true;
+                    break;
+
+                case 3:
+                    AttaqueCaC();
+                    _actionActive = true;
+                    break;
+
+                case 4:
+                    FouDeLaGachette();
+                    _actionActive = true;
+                    break;
             }
         }
     }
@@ -59,5 +100,34 @@ public class Scr_BossPhase1 : MonoBehaviour
         }
 
         _canGoDelay = true;
+    }
+
+    private void TirDeCouverture()
+    {
+        Instantiate(_bullet, _mySelf.position + _player.transform.position.normalized * _shootingAllonge, _mySelf.rotation, _bulletContainer);
+
+        while (_couvertureDuration > 0)
+        {
+            _couvertureDuration -= Time.deltaTime;
+
+            _mySelf.position = Vector2.MoveTowards(_mySelf.position, -_player.transform.position, _couvertureSpeed * Time.deltaTime);
+
+            new WaitForEndOfFrame();
+        }
+    }
+
+    private void Grenade()
+    {
+
+    }
+
+    private void AttaqueCaC()
+    {
+
+    }
+
+    private void FouDeLaGachette()
+    {
+        
     }
 }
