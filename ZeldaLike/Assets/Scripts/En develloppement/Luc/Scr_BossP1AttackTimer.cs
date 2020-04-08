ce9745ac680401124c6f21cc518d3fc7ee525986
LucV <l.vidal@rubika-edu.com>
2020-04-08 16:48:28 +0200

    Boss Phase 1 terminé (sauf le life system que je ne connait pas) demande de vérification de la part du Lead Prog

--- ZeldaLike/Assets/Scripts/En develloppement/Luc/Scr_BossP1AttackTimer.cs
+++ ZeldaLike/Assets/Scripts/En develloppement/Luc/Scr_BossP1AttackTimer.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scr_BossP1AttackTimer : MonoBehaviour
+namespace Ennemies
 {
-    public float _lifeTime;
-    // Start is called before the first frame update
-    void Start()
+    public class Scr_BossP1AttackTimer : MonoBehaviour
     {
-        
-    }
+        public float _lifeTime;
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_lifeTime > 0)
-        {
-            _lifeTime -= Time.deltaTime;
         }
-        else if (_lifeTime <= 0)
+
+        // Update is called once per frame
+        void Update()
         {
-            Destroy(gameObject);
+            if (_lifeTime > 0)
+            {
+                _lifeTime -= Time.deltaTime;
+            }
+            else if (_lifeTime <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
+
--- ZeldaLike/Assets/Scripts/En develloppement/Luc/Scr_BossPhase1.cs
+++ ZeldaLike/Assets/Scripts/En develloppement/Luc/Scr_BossPhase1.cs
@@ -7,66 +7,83 @@ namespace Ennemies
 {
     public class Scr_BossPhase1 : MonoBehaviour
     {
-        private bool _actionActive;
-        public float _delayBetweenActions;
+        [Header("General")]
+        private bool _actionActive; //Pour savoir si une action est en cours.
+        public float _delayBetweenActions;  //Délai avant une nouvelle action.
         private float _delay;
-        private bool _canGoDelay;
-        private int _randomAction;
-        public Transform _mySelf;
-        private GameObject _player;
+        private bool _canGoDelay; //Permet de savoir si on lance le délai entre deux actions.
+        private int _randomAction;  //Détermine l'aléatoire des actions.
+        public Transform _mySelf;  //Le Boss.
+        private GameObject _player; //Le PJ.
+        public float _fightDistance;  //Distance à laquelle le Boss arrête d'avancer.
+        public float _moveSpeed;  //Vitesse de déplacement du Boss.
+        public Vector3 _bossDirection;  //Direction du Boss vers le PJ.
+        public float _retreatDistance;  //Distance à laquelle le Boss recule.
 
         [Header("Renforts")]
-        public List<GameObject> _renforts;
-        public List<Transform> _renfortsSpawns;
+        public List<GameObject> _renforts;  //Liste des ennemis à faire spawn.
+        public List<Transform> _renfortsSpawns;  //Liste des emplacements de spawn des ennemis.
 
-        [Header("Tir de Couverture")]
-        public GameObject _bullet;
-        public float _couvertureSpeed;
-        private float _couvertureDuration;
+        [Header("Tir de Couverture et Fou de la Gachette")]
+        public GameObject _bullet; //Le projectile.
+        public float _couvertureSpeed;  //La vitesse de déplacement du Boss lors de la couverture.
+        private float _couvertureDuration; //La durée du déplacement du Boss lors de la couverture.
         public float _couvertureDurationOrigin;
-        public Transform _bulletContainer;
-        public float _shootingAllonge;
-        public float _delayBetweenShotsOrigin;
-        private float _delayBetweenShots;
-        public List<Vector3> _bulletFury;
-        [HideInInspector] public Vector3 _currentTarget = Vector2.zero;
-
+        public Transform _bulletContainer;  //Parent des projectiles du Boss.
+        public float _shootingAllonge;  // Distance à laquelle apparaissent les projectile (pour pas qu'ils sortent du bide du boss).
+        public float _delayBetweenShotsOrigin;  
+        private float _delayBetweenShots;  // Délai entre les balles de Fou de la Gachette.
+        public List<Vector3> _bulletFury;  // Liste des trajectoires des projectiles de Fou de la Gachette.
+        [HideInInspector] public Vector3 _currentTarget = Vector2.zero;  //Direction actuellement ciblée par le Boss lors de Fou de la Gachette.
+        private bool _couverture;
 
         [Header("Attaque au CaC")]
-        public float _attackRange;
-        public float _attackCast;
-        private Rigidbody2D _playerBody;
-        public float _knockBackSpeed;
-        public float _stunDuration;
-        public GameObject _attackZone;
-
+        public float _attackCast;  //Délai avant l'attaque.
+        public GameObject _attackZone; //Zone touchée par l'attaque.
+        public GameObject _attackPos;  //Point d'apparition de l'attaque.
 
         [Header("Grenade")]
-        public GameObject _grenade;
-        public Vector3 _grenadeTarget;
+        public GameObject _grenade;  //Projectile de Grenade.
+        public Vector3 _grenadeTarget; //Point ciblé par la Grenade.
 
 
         // Start is called before the first frame update
         void Start()
         {
             _player = GameObject.FindGameObjectWithTag("Player");
-            _playerBody = _player.GetComponent<Rigidbody2D>();
             _actionActive = false;
             _delay = _delayBetweenActions;
             _canGoDelay = false;
             _couvertureDuration = _couvertureDurationOrigin;
             _delayBetweenShots = _delayBetweenShotsOrigin;
+            _couverture = false;
 
         }
 
         // Update is called once per frame
         void Update()
         {
+            _bossDirection = (_player.transform.position - _mySelf.position);
+
+            //Déplacement du Boss entre les actions.
             if (_canGoDelay == true)
             {
                 if (_delay > 0)
                 {
                     _delay -= Time.deltaTime;
+
+                    if (Vector2.Distance(_mySelf.position, _player.transform.position) > _fightDistance)
+                    {
+                        _mySelf.position = Vector2.MoveTowards(_mySelf.position, _player.transform.position, _moveSpeed * Time.deltaTime);
+                    }
+                    else if (Vector2.Distance(_mySelf.position, _player.transform.position) < _retreatDistance && Vector2.Distance(_mySelf.position, _player.transform.position) < _fightDistance)
+                    {
+                        _mySelf.position = Vector2.MoveTowards(_mySelf.position, _player.transform.position, -_moveSpeed * Time.deltaTime);
+                    }
+                    else if (Vector2.Distance(_mySelf.position, _player.transform.position) < _fightDistance && Vector2.Distance(_mySelf.position, _player.transform.position) > _retreatDistance)
+                    {
+                        _mySelf.position = _mySelf.position;
+                    }
                 }
                 else if (_delay <= 0)
                 {
@@ -76,6 +93,7 @@ namespace Ennemies
                 }
             }
 
+            //Choix et application des actions.
             if (_actionActive == false)
             {
                 _randomAction = Random.Range(1, 6);
@@ -113,8 +131,23 @@ namespace Ennemies
                         break;
                 }
             }
+
+            //Déplacement de "Tir de couverture".
+            if (_couverture == true && _couvertureDuration > 0)
+            {
+                _couvertureDuration -= Time.deltaTime;
+
+                _mySelf.position = Vector2.MoveTowards(_mySelf.position, _player.transform.position, -_couvertureSpeed * Time.deltaTime);
+            }
+            else if (_couverture == true && _couvertureDuration <= 0)
+            {
+                _couvertureDuration = _couvertureDurationOrigin;
+                _couverture = false;
+                _canGoDelay = true; 
+            }
         }
 
+        //Effet de "Renforts".
         private void Renforts()
         {
             for (int i = 0; i < _renforts.Count; i++)
@@ -125,26 +158,17 @@ namespace Ennemies
             _canGoDelay = true;
         }
 
+        //Effet de "Tir de couverture".
         private void TirDeCouverture()
         {
             _currentTarget = (_player.transform.position - _mySelf.position);
 
             Instantiate(_bullet, _mySelf.position + _currentTarget.normalized * _shootingAllonge, _mySelf.rotation, _bulletContainer);
 
-            while (_couvertureDuration > 0)
-            {
-                _couvertureDuration -= Time.deltaTime;
-
-                _mySelf.position = Vector2.MoveTowards(_mySelf.position, _player.transform.position, -_couvertureSpeed * Time.deltaTime);
-
-                new WaitForEndOfFrame();
-            }
-
-            _couvertureDuration = _couvertureDurationOrigin;
-
-            _canGoDelay = true;
+            _couverture = true; //Permet de lancer le déplacement de "Tir de Couverture" qui se trouve dans l'Update.
         }
 
+        //Effet de "Grenade".
         private void Grenade()
         {
             _grenadeTarget = _player.transform.position;
@@ -154,15 +178,20 @@ namespace Ennemies
             _canGoDelay = true;
         }
 
+        //Effet de "AttaqueCaC".
         private IEnumerator AttaqueCaC()
         {
             yield return new WaitForSeconds(_attackCast);
 
-            Instantiate(_attackZone, _mySelf.position + _player.transform.position.normalized, _mySelf.rotation, _mySelf);
+            Instantiate(_attackZone, _attackPos.transform.position + _bossDirection.normalized * 2, _attackPos.transform.rotation, _attackPos.transform);
+            _attackPos.GetComponent<Scr_BossP1AttackPos>()._attackSet = true;
+
+            yield return new WaitForSeconds(0.5f);
 
             _canGoDelay = true;
         }
 
+        //Effet de "Fou de la Gachette".
         private IEnumerator FouDeLaGachette()
         {
             for (int i = 0; i < _bulletFury.Count; i++)
@@ -173,12 +202,6 @@ namespace Ennemies
             }
 
             _canGoDelay = true;
-        }
-
-        void OnDrawGizmosSelected()
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(_mySelf.position, _attackRange);
         }
 
 
