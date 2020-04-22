ff79f9eb8b9c01f390640cc7d4b5c67da12f2b93
Noé <noe.blanc.2k@gmail.com>
2020-04-22 10:49:47 +0200

    D_E1_P1 prototypé

---
+++ ZeldaLike/Assets/Scripts/En develloppement/Noe/Scr_LD_AlwaysActiveState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Scr_LD_AlwaysActiveState : MonoBehaviour
+    {
+
+        Transform[] allChildren;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            allChildren = GetComponentsInChildren<Transform>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GetComponent<Scr_LD_ActiveState>()._isActive == true)
+            {
+                foreach (Transform child in transform)
+                {
+                    child.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
---
+++ ZeldaLike/Assets/Scripts/En develloppement/Noe/Scr_LD_Timer_ActiveState.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Scr_LD_Timer_ActiveState : MonoBehaviour
+    {
+
+        Transform[] allChildren;
+        public float timer;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            allChildren = GetComponentsInChildren<Transform>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GetComponent<Scr_LD_ActiveState>()._isActive == true)
+            {
+                StartCoroutine(Countdown());
+            }
+        }
+
+        private IEnumerator Countdown()
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+
+            yield return new WaitForSeconds(timer);
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+}
