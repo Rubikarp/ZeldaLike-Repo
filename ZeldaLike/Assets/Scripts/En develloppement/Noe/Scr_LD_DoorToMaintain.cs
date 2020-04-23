3b49bb24854aa7d452c91564037b36c6f31c8ed5
Noé <noe.blanc.2k@gmail.com>
2020-04-23 14:03:21 +0200

    D_E1_P2 prototypé

---
+++ ZeldaLike/Assets/Scripts/En develloppement/Noe/Scr_LD_DoorToMaintain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Scr_LD_DoorToMaintain : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GetComponent<Scr_LD_ActiveState>()._isActive == true)
+            {
+                gameObject.SetActive(false);
+            }
+
+            if (GetComponent<Scr_LD_ActiveState>()._isActive == false)
+            {
+                gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ ZeldaLike/Assets/Scripts/En develloppement/Noe/Scr_LD_Door_AlwaysOpen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Scr_LD_Door_AlwaysOpen : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GetComponent<Scr_LD_ActiveState>()._isActive == true)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
