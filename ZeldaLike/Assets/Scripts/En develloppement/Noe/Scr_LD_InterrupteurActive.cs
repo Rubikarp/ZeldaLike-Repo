38de0282c1278ba9bcacffc09090809754c73adc
Noé <noe.blanc.2k@gmail.com>
2020-04-24 19:42:07 +0200

    D_E2_P1 prototypé

---
+++ ZeldaLike/Assets/Scripts/En develloppement/Noe/Scr_LD_InterrupteurActive.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Scr_LD_InterrupteurActive : MonoBehaviour
+    {
+
+        public bool pressed;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            pressed = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GetComponent<Scr_LD_Interrupteur>()._isActive == true)
+            {
+                pressed = true;
+            }   
+        }
+
+        private void OnDisable()
+        {
+            pressed = false;
+        }
+    }
+}
