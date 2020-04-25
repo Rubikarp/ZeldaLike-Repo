40b39e6c1abfa832136a386d1dd92ca36b352195
LucV <l.vidal@rubika-edu.com>
2020-04-25 18:25:54 +0200

    Combats jusqu'à Balise 5 implémentés

---
+++ ZeldaLike/Assets/Scripts/En develloppement/Luc/Scr_LD_SpawnEnnemies.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_LD_SpawnEnnemies : MonoBehaviour
+{
+    public List<GameObject> _ennemiesToSpawn;
+    public float _spawnDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < _ennemiesToSpawn.Count; i++)
+        {
+            _ennemiesToSpawn[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.transform.parent.parent.CompareTag("Player"))
+        {
+            for (int j = 0; j < _ennemiesToSpawn.Count; j++)
+            {
+                _ennemiesToSpawn[j].SetActive(true);
+            }
+        }
+    }
+}
