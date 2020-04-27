aab4808e06dc34c29569b69ded241da255fd67bc
Louis Vitrant <l.vitrant@rubika-edu.com>
2020-04-27 16:08:37 +0200

    Items créés
    Sont disponible dans Prefab > Items

---
+++ ZeldaLike/Assets/Scripts/Items/Scr_Items_BoostHP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Management;
+
+namespace Game
+{
+    public class Scr_Items_BoostHP : MonoBehaviour
+    {
+        public GameObject _boostItems;
+        public Scr_PlayerLifeSystem _playerLife;
+
+        private void Start()
+        {
+            _boostItems = this.gameObject;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.transform.parent.parent.CompareTag("Player") && _playerLife._maxlife <= 9)
+            {
+                _playerLife._maxlife = _playerLife._maxlife + 1;
+                Destroy(_boostItems);
+            }
+        }
+    }
+}
---
+++ ZeldaLike/Assets/Scripts/Items/Scr_Items_Soin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Management;
+
+namespace Game
+{
+    public class Scr_Items_Soin : MonoBehaviour
+    {
+        public GameObject _healthItems;
+        public Scr_PlayerLifeSystem _playerLife;
+        public int _regenerationCapacity = 1;
+
+        private void Start()
+        {
+            _healthItems = this.gameObject;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.transform.parent.parent.CompareTag("Player") && _playerLife._life < _playerLife._maxlife)
+            {
+                _playerLife._life = _playerLife._life + _regenerationCapacity;
+                Destroy(_healthItems);
+            }
+            else if(collision.gameObject.transform.parent.parent.CompareTag("Player") && _playerLife._life == 6)
+            {
+                Destroy(_healthItems);
+            }
+        }
+    }
+}
