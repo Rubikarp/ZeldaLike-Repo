387658405b82866067d6303b0b019b61b721ae9d
LucV <l.vidal@rubika-edu.com>
2020-05-01 11:54:51 +0200

    Correction puzzle E2_P2

--- ZeldaLike/Assets/Scripts/LD/Scr_ActivatingSecondDoorE2.cs
+++ ZeldaLike/Assets/Scripts/LD/Scr_ActivatingSecondDoorE2.cs
@@ -7,8 +7,8 @@ namespace Game
     public class Scr_ActivatingSecondDoorE2 : MonoBehaviour
     {
         public GameObject _lockedDoor = null;
-        public GameObject _openedDoor = null;
-        public Scr_LD_Interrupteur[] _interruptors = null;
+        //public GameObject _openedDoor = null;
+        public Scr_LD_ActiveState[] _interruptors = null;
 
         private void Start()
         {
@@ -19,12 +19,12 @@ namespace Game
         {
             if(_interruptors[0]._isActive && _interruptors[1]._isActive && _interruptors[2]._isActive)
             {
-                _openedDoor.SetActive(true);
+                //_openedDoor.SetActive(true);
                 _lockedDoor.SetActive(false);
             }
             else
             {
-                _openedDoor.SetActive(false);
+                //_openedDoor.SetActive(false);
                 _lockedDoor.SetActive(true);
             }
         }
