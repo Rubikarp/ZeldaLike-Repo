efd09dca33bdaf4dcba5223619dbbc81b1572fd2
LucV <l.vidal@rubika-edu.com>
2020-05-02 17:52:42 +0200

    Situations de combats des donjons

--- ZeldaLike/Assets/Scripts/En develloppement/Noe/Scr_Ennemis_D_E2_P1.cs
+++ ZeldaLike/Assets/Scripts/En develloppement/Noe/Scr_Ennemis_D_E2_P1.cs
@@ -6,23 +6,18 @@ namespace Game
 {
     public class Scr_Ennemis_D_E2_P1 : MonoBehaviour
     {
-        public GameObject _soldat;
         public GameObject _ingenieur;
         public GameObject _harceleur;
         public GameObject _expRat1;
         public GameObject _expRat2;
-        public GameObject _expRat3;
-        public GameObject _expRat4;
+
 
         public bool appearing;
 
-        public Vector2 soldatPosition;
         public Vector2 ingenieurPosition;
         public Vector2 harceleurPosition;
         public Vector2 expRat1Position;
         public Vector2 expRat2Position;
-        public Vector2 expRat3Position;
-        public Vector2 expRat4Position;
 
         private float timerEnnemy;
 
@@ -47,24 +42,18 @@ namespace Game
         {
             appearing = true;
             
-            GameObject soldat = (GameObject) Instantiate(_soldat, soldatPosition, Quaternion.identity);
             GameObject ingenieur = (GameObject)Instantiate(_ingenieur, ingenieurPosition, Quaternion.identity);
             GameObject harceleur = (GameObject)Instantiate(_harceleur, harceleurPosition, Quaternion.identity);
             Debug.Log("truc");
             GameObject expRat1 = (GameObject)Instantiate(_expRat1, expRat1Position, Quaternion.identity);
             GameObject expRat2 = (GameObject)Instantiate(_expRat2, expRat2Position, Quaternion.identity);
-            GameObject expRat3 = (GameObject)Instantiate(_expRat3, expRat3Position, Quaternion.identity);
-            GameObject expRat4 = (GameObject)Instantiate(_expRat4, expRat4Position, Quaternion.identity);
 
             yield return new WaitForSecondsRealtime(timerEnnemy);
 
-            Destroy(soldat);
             Destroy(ingenieur);
             Destroy(harceleur);
             Destroy(expRat1);
             Destroy(expRat2);
-            Destroy(expRat3);
-            Destroy(expRat4);
 
             appearing = false;
 
