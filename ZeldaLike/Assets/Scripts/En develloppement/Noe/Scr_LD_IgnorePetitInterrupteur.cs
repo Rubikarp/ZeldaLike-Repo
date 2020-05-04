82c1edf86b91a89d9009ddebaa5ce3210dbc6aa2
Noé <noe.blanc.2k@gmail.com>
2020-05-04 11:47:32 +0200

    Avancées E3_P2

---
+++ ZeldaLike/Assets/Scripts/En develloppement/Noe/Scr_LD_IgnorePetitInterrupteur.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_LD_IgnorePetitInterrupteur : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PetitInterrupteur")
+        {
+            Physics.IgnoreCollision(collision.collider, collider);
+        }
+    }
+}
