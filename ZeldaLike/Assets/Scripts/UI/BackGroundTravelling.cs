f7935cd348904a2e016b4721f89b15bb1fd30c11
Deleye Arthur <arthurdeleye@gmail.com>
2020-05-13 22:39:56 +0200

    Début de menu car j'ai pas les buttons

---
+++ ZeldaLike/Assets/Scripts/UI/BackGroundTravelling.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game
+{
+    public class BackGroundTravelling : MonoBehaviour
+    {
+        [Header("Travelling")]
+        public LeanTweenType easeType;
+        public AnimationCurve curve;
+        [SerializeField] private float finalHeight = 0f;
+        [SerializeField] private float travelingTime = 5f;
+
+        [Header("Event")]
+        public UnityEvent uEvent = null ;
+
+        private void Start()
+        {
+            if(easeType == LeanTweenType.animationCurve)
+            {
+                LeanTween.moveY(gameObject, finalHeight, travelingTime).setLoopOnce().setEase(curve);
+            }
+            else
+            {
+                LeanTween.moveY(gameObject, finalHeight, travelingTime).setLoopOnce().setEase(easeType);
+            }
+
+            Invoke("Evenement", travelingTime * 0.66f);
+        }
+
+        public void Evenement() 
+        {
+            uEvent?.Invoke();
+        } 
+    }
+}
