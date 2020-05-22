c55ab99c1ac15d351a187399739fcb3c671f6ec8
LucV <l.vidal@rubika-edu.com>
2020-05-22 18:15:59 +0200

    Les combats trigger désormais un collider qui disparaît une fois les ennemis morts

--- ZeldaLike/Assets/Scripts/LD/Scr_LD_SpawnEnnemies.cs
+++ ZeldaLike/Assets/Scripts/LD/Scr_LD_SpawnEnnemies.cs
@@ -6,6 +6,10 @@ public class Scr_LD_SpawnEnnemies : MonoBehaviour
 {
     public List<GameObject> _ennemiesToSpawn;
     public float[] _spawnDelay;
+    public GameObject _limitTilemap;
+    public GameObject _limitCollider;
+    private bool _fightStarted;
+    private int _killCount;
 
     // Start is called before the first frame update
     void Start()
@@ -14,17 +18,37 @@ public class Scr_LD_SpawnEnnemies : MonoBehaviour
         {
             _ennemiesToSpawn[i].SetActive(false);
         }
+
+        _fightStarted = false;
+        _killCount = 0;
+        _limitCollider.SetActive(false);
+        _limitTilemap.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_fightStarted == true)
+        {
+            for (int k = 0; k < _ennemiesToSpawn.Count; k++)
+            {
+                if (_ennemiesToSpawn[k] == null && _killCount == k)
+                {
+                    _killCount += 1;
+                }
+            }
+
+            if (_killCount == _ennemiesToSpawn.Count)
+            {
+                _limitCollider.SetActive(false);
+                _limitTilemap.SetActive(false);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && _fightStarted == false)
         {
             StartCoroutine(EnemySpawn());
         }
@@ -32,6 +56,10 @@ public class Scr_LD_SpawnEnnemies : MonoBehaviour
 
     private IEnumerator EnemySpawn()
     {
+        _fightStarted = true;
+        _limitCollider.SetActive(true);
+        _limitTilemap.SetActive(true);
+
         for (int j = 0; j < _ennemiesToSpawn.Count; j++)
         {
             yield return new WaitForSeconds(_spawnDelay[j]);
