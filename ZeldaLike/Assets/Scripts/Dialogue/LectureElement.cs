1f3178b3811bb6b4525dfcb3586d7783b30aa4d3
Deleye Arthur <arthurdeleye@gmail.com>
2020-05-23 12:46:15 +0200

    élément de texte
    avec exemple dans la scène de louis

---
+++ ZeldaLike/Assets/Scripts/Dialogue/LectureElement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Dialogue
+{
+    public class LectureElement : MonoBehaviour
+    {
+        [Header("Component")]
+        private DialogueManager dialogManag;
+        public GameObject ActivationButton;
+
+        private void Start()
+        {
+            dialogManag = GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<DialogueManager>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                ActivationButton.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                ActivationButton.SetActive(false);
+            }
+        }
+
+        public void Lecture(Conversation Text)
+        {
+            {
+                dialogManag = GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<DialogueManager>();
+                dialogManag.BeginCoversation(Text);
+                dialogManag.isAuto = true;
+            }
+        }
+
+    }
+}
