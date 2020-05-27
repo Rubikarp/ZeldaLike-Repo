40f389248acaabb4b8a9f77b431805944cf6d06b
LucV <l.vidal@rubika-edu.com>
2020-05-27 18:51:53 +0200

    Fignolage Boss P3

--- ZeldaLike/Assets/Scripts/Ennemis/Boss/Boss Phase 3/Scr_BossP3_Bullet.cs
+++ ZeldaLike/Assets/Scripts/Ennemis/Boss/Boss Phase 3/Scr_BossP3_Bullet.cs
@@ -9,40 +9,58 @@ namespace Ennemis
         [Header("Data")]
         public Transform _projectile = null;
         public Rigidbody2D _rb2d = null;
+        private float _rotZ;
+        public Animator _anim;
 
         [Header("Statistiques")]
         public Vector2 _BulletDir = Vector2.zero;
         public float _mySpeed = 12f;
         public float _timer = 3f;
+        private bool _isDying;
 
         private void Start()
         {
+            _isDying = false;
             _projectile = this.transform;
             _rb2d = this.GetComponent<Rigidbody2D>();
+            _rotZ = Mathf.Atan2(_BulletDir.y, _BulletDir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, _rotZ + 90);
             Destroy(this.gameObject, _timer);
         }
 
         private void Update()
         {
-            _rb2d.position += _BulletDir * _mySpeed * Time.deltaTime;
+            if (!_isDying)
+            {
+                _rb2d.position += _BulletDir * _mySpeed * Time.deltaTime;
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.CompareTag("Player/HurtBox"))
             {
-                Destroy(gameObject);
+                StartCoroutine(BulletDestroy());
             }
             else if (collision.gameObject.CompareTag("Environment"))
             {
                 Debug.Log(collision.gameObject.name);
-                Destroy(gameObject);
+                StartCoroutine(BulletDestroy());
             }
         }
 
         public void BulletSetDir(Vector2 direction)
         {
             _BulletDir = direction;
+        }
+
+        private IEnumerator BulletDestroy()
+        {
+            _isDying = true;
+            _anim.SetTrigger("Destroy");
+            _rb2d.velocity = Vector2.zero;
+            yield return new WaitForSeconds(0.25f);
+            Destroy(gameObject);
         }
 
     }
