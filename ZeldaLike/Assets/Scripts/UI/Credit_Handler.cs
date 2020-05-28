a197e663e549dabe896133ee1999b6134b031f6f
Deleye Arthur <arthurdeleye@gmail.com>
2020-05-28 11:45:16 +0200

    Crédit défilant fait !

---
+++ ZeldaLike/Assets/Scripts/UI/Credit_Handler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Credit_Handler : MonoBehaviour
+    {
+        [Header("Travelling")]
+        [SerializeField] private GameObject CreditList = null;
+        [SerializeField] private float startHeight = 0f;
+        [SerializeField] private float finalHeight = 4000f;
+        [SerializeField] private float travelingTime = 10f;
+
+        private void OnEnable()
+        {
+            Time.timeScale = 1;
+            CreditList.transform.position = new Vector3(960, startHeight, 0);
+            LeanTween.moveY(CreditList, finalHeight, travelingTime);
+
+        }
+    }
+}
