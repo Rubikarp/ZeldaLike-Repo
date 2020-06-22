3d6917b08f84a0c975b93ca65701b9a9a449fe9a
Deleye Arthur <arthurdeleye@gmail.com>
2020-06-22 23:40:29 +0200

    bien se passer

---
+++ Nahua - Projet Unity/Assets/Scripts/Ennemis/Ennemis_Ingénieur/Scr_ProjectileBehaviour_Ingénieur.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Ennemis
+{
+    public class Scr_ProjectileBehaviour_Ingénieur : MonoBehaviour
+    {
+        [Header("Data")]
+        public Transform _projectile = null;
+        public Rigidbody2D _rb2d = null;
+
+        [Header("Statistiques")]
+        public Vector2 _BulletDir = Vector2.zero;
+        public float _mySpeed = 10f;
+        public float _timer = 1f;
+        private float _rotZ;
+
+        private void Start()
+        {
+            _projectile = this.transform;
+            _rb2d = this.GetComponent<Rigidbody2D>();
+            _rotZ = Mathf.Atan2(_BulletDir.y, _BulletDir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, _rotZ + 90);
+            Destroy(this.gameObject, _timer);
+        }
+
+        private void Update()
+        {
+            _rb2d.position += _BulletDir * _mySpeed * Time.deltaTime;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player/HurtBox"))
+            {
+                Destroy(gameObject);
+            }
+            else if (collision.gameObject.CompareTag("Environment"))
+            {
+                Debug.Log(collision.gameObject.name);
+                Destroy(gameObject);
+            }
+        }
+
+        public void BulletSetDir(Vector2 direction)
+        {
+            _BulletDir = direction;
+        }
+
+    }
+}
